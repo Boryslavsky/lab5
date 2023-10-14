0013f717e653c82581ff4297016dba48551d6ec8
Boryslavsky <pavloboryslavsky@gmail.com>
2023-10-14 13:32:20 +0300

    код і файл

--- main.py
+++ main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import re
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def read_file(filename):
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            text = file.read()
+            return text
+    except FileNotFoundError:
+        print(f"Файл '{filename}' не знайдено.")
+        return None
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def extract_first_sentence(text):
+    sentences = re.split(r'(?<=[.!?])\s', text)
+    if sentences:
+        return sentences[0]
+    return None
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def clean_and_sort_words(text):
+    ukrainian_words = []
+    english_words = []
+
+    words = re.findall(r'\w+', text, re.U)
+    for word in words:
+        if re.search(r'[а-яА-Я]', word):
+            ukrainian_words.append(word)
+        else:
+            english_words.append(word)
+
+    ukrainian_words = sorted(ukrainian_words, key=lambda word: (word.lower(), word))
+    english_words = sorted(english_words, key=lambda word: (word.lower(), word))
+
+    return ukrainian_words + english_words
+
+
+def count_words(text):
+    words = re.findall(r'\w+', text, re.U)
+    return len(words)
+
+
+filename = 'file.txt'
+text = read_file(filename)
+
+if text:
+    first_sentence = extract_first_sentence(text)
+    if first_sentence:
+        print("Перше речення:")
+        print(first_sentence)
+
+        words = clean_and_sort_words(text)
+        print("\nВсі слова в тексті (відсортовані по алфавіту):")
+        for word in words:
+            print(word)
+
+        word_count = count_words(text)
+        print(f"\nКількість слів в тексті: {word_count}")
